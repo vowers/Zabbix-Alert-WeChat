c31be8aae22556de6e93f0b2d917d873a664b25a
vowers <vowers@126.com>
2017-08-11 11:27:05 +0800

    修复向部门群发的bug

--- wechat.py
+++ wechat.py
@@ -15,11 +15,12 @@ def GetToken(Corpid,Secret):
     Token = r.json()['access_token']
     return Token
 
-def SendMessage(Token,User,Agentid,Subject,Content):
+def SendMessage(Token,User,Partid,Agentid,Subject,Content):
     Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
     Data = {
-        "touser": User,                                 # 企业号中的用户帐号，在zabbix用户Media中配置，如果配置不正常，将按部门发送。
-        #"totag": Tagid,                                # 企业号中的部门id，群发时使用。
+        "touser": User,                                 # 成员ID列表（消息接收者，多个接收者用‘|’分隔，最多支持1000个）。特殊情况：指定为@all，则向该企业应用的全部成员发送
+        "toparty" : Partid,                             # 部门ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为@all时忽略本参数
+        #"totag": Tagid,                                # 标签ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为@all时忽略本参数
         "msgtype": "text",                              # 消息类型。
         "agentid": Agentid,                             # 企业号中的应用id。
         "text": {
@@ -36,11 +37,11 @@ if __name__ == '__main__':
     Subject = sys.argv[2]                                                           # zabbix传过来的第二个参数
     Content = sys.argv[3]                                                           # zabbix传过来的第三个参数
 
-    Corpid = "wx0f0c"                                                   # CorpID是企业号的标识
-    Secret = "cecnGXBY"     # Secret是管理组凭证密钥
-    #Tagid = "1"                                                                     # 通讯录标签ID
+    Corpid = "xxxxxxxxxxxx"                                                               # CorpID是企业号的标识
+    Secret = "xxxxxxxxxxxxxx"                                                             # Secret是管理组凭证密钥
+    #Tagid = "1"                                                                     
     Agentid = "1"                                                                   # 应用ID
-
+    Partid = "1"                                                                    # 通讯录标签ID
     Token = GetToken(Corpid, Secret)
-    Status = SendMessage(Token,User,Agentid,Subject,Content)
+    Status = SendMessage(Token,User,Partid,Agentid,Subject,Content)
     print Status
